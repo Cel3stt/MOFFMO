30dd7e3490b701e2da37e3bd171f96296d1c8d2f
Cel3stt <mariannecelest@gmail.com>
2025-05-18 20:46:06 +0800

    change logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useAuthStore } from '../store/useAuthStore'
 import { Link } from 'react-router-dom'
-import { LogOut, MessageSquare, Settings, User } from 'lucide-react'
+import { LogOut, Settings, Star, User } from 'lucide-react'
 
 const Navbar = () => {
 
@@ -13,9 +13,9 @@ const Navbar = () => {
         <div className="flex justify-between items-center h-full">
           <Link to='/' className='flex items-center gap-2.5 hover:opacity-80 transition-all'>
           <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-          <MessageSquare className='w-5 h-5 text-primary'/>
+          <Star className='w-5 h-5 text-primary'/>
           </div>
-          <h1 className='text-lg font-bold'>MOFFMO</h1>
+          <h1 className='text-3xl font-bold logo '>Moffmo</h1>
           </Link>
         
 
