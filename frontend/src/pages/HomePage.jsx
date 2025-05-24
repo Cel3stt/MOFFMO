304d4cea292c355b4bbe95e8fb7d2b4e41c0e373
Cel3stt <mariannecelest@gmail.com>
2025-05-25 00:21:45 +0800

    change some designs

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,8 +10,8 @@ const HomePage = () => {
   const {selectedUser} = useChatStore()
   return (
     <div className="h-screen " style={{ backgroundImage: `url(${bglight})` , backgroundSize: 'cover', backgroundPosition: 'center'}}>
-      <div className="flex items-center justify-center pt-20 container mx-auto">
-        <div className="bg-base-100 rounded-lg shadow-lg w-full max-w-full h-[calc(100vh-8rem)]">
+         <div className="flex justify-center pt-20 px-2 md:px-6 lg:px-12">
+  <div className="bg-base-100 rounded-lg shadow-lg w-full max-w-[1400px] h-[85vh]">
           <div className="flex h-full rounded-lg overflow-hidden">
 
             <Sidebar/>
