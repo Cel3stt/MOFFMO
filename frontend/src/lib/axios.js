2f96a3e8065c644784c67bd38eec4e903748dd64
Cel3stt <mariannecelest@gmail.com>
2025-06-06 04:32:30 +0800

    test

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://moffmo-1.onrender.com', // http://localhost:3000/api
     withCredentials: true,
 })
