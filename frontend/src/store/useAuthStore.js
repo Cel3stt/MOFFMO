cc6df31e2cdf818925d3b6662c7161c98aa5fa65
Cel3stt <mariannecelest@gmail.com>
2025-06-06 04:37:29 +0800

    test

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast';
 
 import {io} from 'socket.io-client'
 
-const BASE_URL = 'https://moffmo-1.onrender.com' // 'http://localhost:3000'
+const BASE_URL = 'http://localhost:3000'
 
 export const useAuthStore = create((set,get) => ({
     authUser: null,
