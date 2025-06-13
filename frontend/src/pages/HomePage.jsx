ef8b23b9ae845b2a3a85f64acd5b241cbb039b37
Cel3stt <mariannecelest@gmail.com>
2025-06-13 22:36:53 +0800

    change roundness

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
     <div className="h-screen " style={{ backgroundImage: `url(${bglight})` , backgroundSize: 'cover', backgroundPosition: 'center'}}>
          <div className="flex justify-center pt-20 px-2 md:px-6 lg:px-12">
   <div className="bg-base-100 rounded-lg shadow-lg w-full max-w-[1400px] h-[85vh]">
-          <div className="flex h-full rounded-lg overflow-hidden">
+          <div className="flex h-full rounded-md overflow-hidden">
 
             <Sidebar/>
 
